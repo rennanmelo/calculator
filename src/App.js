0554c112cd6b26fbd21e8b6b1deb7ee4ab88eac3
Rennan Melo <rennan.melo11@gmail.com>
2019-09-29 06:02:23 -0300

    Finish the basic calculator

--- src/App.js
+++ src/App.js
@@ -8,30 +8,66 @@ class App extends Component {
 
         this.state = {
             current: "0",
-            previous: []
+            previous: [],
+            nextIsReset: false
         }
     }
+    
+    // To do, calcular realizando podendo usar numeros negativos first e nao ocorrer erro quando clicar mais de uma vez nos operadores e/ou clicar neles depois do igual
 
     reset = () => {
-        this.setState({
-            current: "0"
-        })
+        this.setState({current: "0", previous: [], nextIsReset: false})
     }
 
     addToCurrent = (symbol) => {
-        this.setState({
-            current: this.state.current + symbol
-        })
+        if(['/', '*', '+', '-'].indexOf(symbol) > -1 ) {
+            let {previous} = this.state
+            console.log(['/', '*', '+', '-'].indexOf(symbol))
+            previous.push(this.state.current + symbol)
+            this.setState({previous, nextIsReset: true}) // previous: previous
+        } else {
+            if ((this.state.current === "0" && symbol !== '.') || this.state.nextIsReset) {
+                this.setState({current: symbol, nextIsReset: false})
+            } else {
+                this.setState({
+                    current: this.state.current + symbol})
+            }
+        }
     }
 
+    addToCalculate = (symbol) => {
+        let {current, previous, nextIsReset} = this.state
+        if(previous.length > 0) {
+            current = eval(String(previous[previous.length - 1] + current))
+            this.setState({current, previous: [], nextIsReset: true})
+        }
+    }
+/*
+    changeSign = () => {
+        let {current, previous} = this.state
+        if (current.includes('-')) {
+            let sign = '+'
+            let newCurrent = sign.concat(current)
+            this.setState({current: newCurrent})
+
+            console.log("It's a negative number")
+        } else {
+            let sign = '-'
+            let newCurrent = sign.concat(current)            
+            console.log("It's a positive number")
+            this.setState({current: newCurrent})
+        }
+    }
+*/
     render() {
         const buttons = [
-            {symbol: 'C', cols: 3, action: this.reset},
+            {symbol: 'C', cols: 2, action: this.reset},
+            {symbol: '+-', cols: 1, action: this.changeSign},
             {symbol: '/', cols: 1, action: this.addToCurrent},
             {symbol: '7', cols: 1, action: this.addToCurrent},
             {symbol: '8', cols: 1, action: this.addToCurrent},
             {symbol: '9', cols: 1, action: this.addToCurrent},
-            {symbol: 'X', cols: 1, action: this.addToCurrent},
+            {symbol: '*', cols: 1, action: this.addToCurrent},
             {symbol: '4', cols: 1, action: this.addToCurrent},
             {symbol: '5', cols: 1, action: this.addToCurrent},
             {symbol: '6', cols: 1, action: this.addToCurrent},
@@ -42,17 +78,20 @@ class App extends Component {
             {symbol: '-', cols: 1, action: this.addToCurrent},
             {symbol: '0', cols: 2, action: this.addToCurrent},
             {symbol: '.', cols: 1, action: this.addToCurrent},
-            {symbol: '=', cols: 1, action: this.addToCurrent},
-
+            {symbol: '=', cols: 1, action: this.addToCalculate},
         ]
         return (
-            <div>
+            <div className="calculator">
+                {this.state.current.length > 0 ?
+                    <div className="floaty-last">{this.state.previous[this.state.previous.length - 1]}</div>
+                : null
+                }
+
                 <input className="result" type="text" value={this.state.current} />
                 {
                     buttons.map((btn, i) => {
-                        return <Button symbol={btn.symbol} cols={btn.cols} action={symbol => btn.action} />
+                        return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={symbol => btn.action(symbol)} />
                     })
-
                 }
             </div>
         )
--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
+import './css/style.css'
 
 class Button extends Component {
     render() {
         return (
-            <button className="calc-button">{this.props.symbol}</button>
+            <div className={`column-${this.props.cols}`}>
+                <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
+            </div>
         )
     }
 
